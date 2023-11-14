497c0bbd1f2b84c2afbf7bed93d6f67b552d94ee
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-15 00:15:15 +0100

    feat/base_code : Obstacle duration

--- src/environment/obstacle.py
+++ src/environment/obstacle.py
@@ -3,11 +3,12 @@ from src.utils.position import Position
 
 
 class Obstacle:
-    def __init__(self, id: str, position: Position, width: int, height: int) -> None:
+    def __init__(self, id: str, position: Position, width: int, height: int, duration:int) -> None:
         self.id = id
         self.position = position
         self.width = width
         self.height = height
+        self.duration = duration
 
 
     def is_in(self, position: Position) -> bool:
