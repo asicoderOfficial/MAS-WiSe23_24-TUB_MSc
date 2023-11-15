99254ccd6a534dc3d07885272443645ca72164c4
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-15 20:10:34 +0100

    feat/base_code : ENTITIES_TO_KEYS constant

--- src/constants/environment.py
+++ src/constants/environment.py
@@ -1,7 +1,19 @@
+from src.agents.agent import Agent
+
+from src.environment.package import Package
+from src.environment.obstacle import Obstacle
+from src.environment.package_point import PackagePoint
 
 EMPTY_CELL = {
     'agents' : {},
     'obstacles' : {},
     'package_points' : {},
     'packages' : {}
+}
+
+ENTITIES_TO_KEYS = {
+    Agent: 'agents',
+    Obstacle: 'obstacles',
+    Package: 'packages',
+    PackagePoint: 'package_points'
 }
