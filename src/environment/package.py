1ef9c26a22e5e12d6c7174ac8295483e52c71acb
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-15 20:36:19 +0100

    feat/base_code : Package destination

--- src/environment/package.py
+++ src/environment/package.py
@@ -3,9 +3,10 @@ from src.utils.position import Position
 
 
 class Package:
-    def __init__(self, id: str, position: Position, max_iterations_to_deliver: int) -> None:
+    def __init__(self, id: str, position: Position, destination: Position, max_iterations_to_deliver: int) -> None:
         self.id = id
         self.position = position
+        self.destination = destination
         self.max_iterations_to_deliver = max_iterations_to_deliver
         self.iterations = 0
         self.is_delayed = False
