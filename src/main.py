ef217c061603e124e2c3ea5e719a903271ed19c0
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-15 20:37:02 +0100

    feat/base_code : main.py adjusted accordingly

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from src.agents.agent import Agent
 
 from src.utils.position import Position
-from src.utils.objects_parser import object_to_dict
 from src.environment.package import Package
 from src.environment.package_point import PackagePoint
 from src.environment.obstacle import Obstacle
@@ -9,14 +8,14 @@ from src.environment.obstacle import Obstacle
 from src.environment.environment import Environment
 
 # Environment elements
-starting_package = Package('p1', Position(1, 1),  10)
+starting_package = Package('p1', Position(1, 2), Position(1, 2), 10)
 
 starting_package_point = PackagePoint('pp1', Position(1, 1))
 
 starting_obstacle = Obstacle('o1', Position(2, 2), 1, 1, 1)
 
 starting_position = Position(0, 0)
-a = Agent('a', starting_position, 'p1')
+a = Agent('a', starting_position, starting_package)
 
 # Environment
 #environment = Environment(5, 5, [a], [], [], [])
