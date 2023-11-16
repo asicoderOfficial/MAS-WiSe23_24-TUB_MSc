5d2bbdbf4700d7f07d3d063a2f2e28f97a577446
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-16 23:34:58 +0100

    feat/base_code : Package docu + args implement

--- src/environment/package.py
+++ src/environment/package.py
@@ -3,7 +3,19 @@ from src.utils.position import Position
 
 
 class Package:
+    """ A package that the agent can carry, or that can be stored in the PackagePoint."""
     def __init__(self, id: str, position: Position, destination: Position, max_iterations_to_deliver: int) -> None:
+        """ Constructor.
+
+        Args:
+            id (str): ID to identify the package.
+            position (Position): Position of the package in the environment.
+            destination (Position): Position where the package has to be delivered. It is determined initially by the code that runs the experiment. It will not change. It is a PackagePoint.
+            max_iterations_to_deliver (int): Maximum number of iterations the agent has to deliver the package.
+        
+        Returns:
+            None
+        """        
         self.id = id
         self.position = position
         self.destination = destination
@@ -13,5 +25,15 @@ class Package:
     
 
     def step(self, new_position: Position) -> None:
-        """ Method called at each iteration of the simulation."""
+        """ The package position and internal state are updated.
+
+        Args:
+            new_position (Position): The new position of the package in the environment.
+        
+        Returns:
+            None
+        """        
         self.position = new_position
+        self.iterations += 1
+        if self.iterations >= self.max_iterations_to_deliver:
+            self.is_delayed = True
