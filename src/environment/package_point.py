8a1f6ccdfae3d443b58340e5514dac817eaa8d7e
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-16 23:29:16 +0100

    feat/base_code : Package point documentation

--- src/environment/package_point.py
+++ src/environment/package_point.py
@@ -4,7 +4,15 @@ from src.utils.position import Position
 
 
 class PackagePoint:
+    """ A point where the agent can leave a package, or pick up from."""
     def __init__(self, id: str, position: Position, max_simultaneous_packages_storage:float=float('inf')) -> None:
+        """ Constructor.
+
+        Args:
+            id (str): ID to identify the package point.
+            position (Position): Position of the package point in the environment.
+            max_simultaneous_packages_storage (float, optional): Maximum number of packages that can be stored simultaneously. Defaults to float('inf').
+        """        
         self.id = id
         self.position = position
         self.max_simultaneous_packages_storage = max_simultaneous_packages_storage
