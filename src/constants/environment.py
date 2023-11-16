141b87851a9ece4192cf1c1af392c9839cbe545f
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-16 22:39:55 +0100

    feat/base_code : Constants refined

--- src/constants/environment.py
+++ src/constants/environment.py
@@ -4,16 +4,21 @@ from src.environment.package import Package
 from src.environment.obstacle import Obstacle
 from src.environment.package_point import PackagePoint
 
+AGENT_KEY = 'agents'
+PACKAGE_KEY = 'packages'
+OBSTACLE_KEY = 'obstacles'
+PACKAGE_POINT_KEY = 'package_points'
+
 EMPTY_CELL = {
-    'agents' : {},
-    'obstacles' : {},
-    'package_points' : {},
-    'packages' : {}
+    AGENT_KEY : {},
+    OBSTACLE_KEY : {},
+    PACKAGE_POINT_KEY : {},
+    PACKAGE_KEY : {}
 }
 
 ENTITIES_TO_KEYS = {
-    Agent: 'agents',
-    Obstacle: 'obstacles',
-    Package: 'packages',
-    PackagePoint: 'package_points'
+    Agent : AGENT_KEY,
+    Package : PACKAGE_KEY,
+    Obstacle : OBSTACLE_KEY,
+    PackagePoint : PACKAGE_POINT_KEY
 }
