64f27ef36fe4f644441e6e50aab0db328111c8ce
Maxim Popov <itsmail4max@gmail.com>
2023-11-17 15:31:21 +0100

    add position dist_to function

--- src/utils/position.py
+++ src/utils/position.py
@@ -4,3 +4,15 @@ class Position:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
+        
+    def dist_to(self, another_position) -> int:
+        """Get Manhattan distance to another position
+
+        Args:
+            another_position (Position): another position object
+
+        Returns:
+            int: Manhattan distance to passed position
+        """
+        return abs(self.x - another_position.x) + abs(self.y - another_position.y)
+        
