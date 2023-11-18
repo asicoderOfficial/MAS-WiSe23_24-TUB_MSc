7d69b70fda3eb48f48f2881bca5b234fb506abac
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-18 01:07:06 +0100

    feat/base_code : to merge

--- src/environment/environment.py
+++ src/environment/environment.py
@@ -1,16 +1,12 @@
-from typing import List, Union
-from copy import deepcopy
+from typing import List
 
 from mesa import Model
-
+from mesa.space import MultiGrid
 
 from src.agents.agent import Agent
 from src.environment.package import Package
 from src.environment.package_point import PackagePoint
 from src.environment.obstacle import Obstacle
-from src.utils.position import Position
-from src.utils.objects_parser import object_to_dict
-from src.constants.environment import EMPTY_CELL, ENTITIES_TO_KEYS, AGENT_KEY, PACKAGE_KEY, OBSTACLE_KEY, PACKAGE_POINT_KEY
 
 
 class Environment(Model):
@@ -32,14 +28,14 @@ class Environment(Model):
         """        
         self.grid_height = grid_height
         self.grid_width = grid_width
-        self.agents = {agent.id: agent for agent in agents}
-        self.package_points = {package_point.id: package_point for package_point in package_points}
-        self.obstacles = {obstacle.id: obstacle for obstacle in obstacles}
-        self.packages = {package.id: package for package in packages}
+        self.agents = agents
+        self.package_points = package_points
+        self.obstacles = obstacles
+        self.packages = packages
 
         # Create self.grid with grid_height rows and grid_width columns
-        self.grid = [[deepcopy(EMPTY_CELL) for _ in range(self.grid_width)] for _ in range(self.grid_height)]
-        self.init_grid()
+        self.grid = MultiGrid(width=self.grid_width, height=self.grid_height, torus=False)
+        self.init_grid(agents, package_points, packages, obstacles)
 
 
     def step(self, current_iteration: int) -> None:
@@ -51,80 +47,37 @@ class Environment(Model):
         Returns:
             None 
         """        
-        previous_agents_positions = {agent.id: agent.position for agent in self.agents.values()}
-        previous_packages_positions = {package.id: package.position for package in self.packages.values()}
-        for _, agent_object in self.agents.items():
-            agent_object.step(self.grid)
-            if agent_object.package:
+        for agent in self.agents:
+            agent.step(self.grid)
+            if agent.package:
                 # The agent package_id != '', so the agent is carrying the package. So wherever the agent goes, the package goes too.
-                self.packages[agent_object.package.id].step(agent_object.position)
+                agent.package.step(agent.pos, self.grid)
         
-        for _, obstacle_object in self.obstacles.items():
-            obstacle_object.step(current_iteration)
-        
-        self.update_grid(previous_agents_positions, previous_packages_positions, current_iteration)
+        for obstacle in self.obstacles:
+            obstacle.step(current_iteration, self.grid)
 
-
-    def _update_entity_position(self, entity: Union[Agent, Package], previous_position: Position) -> None:
-        """ Updates the position of an entity in the grid, based on entity's internal position
-
-        Args:
-            entity (Union[Agent, Package]): The dynamic entity to update its position (Agent or Package)
-            previous_position (Position): The previous position of the entity.
-        
-        Returns:
-            None 
-        """        
-        entity_key = next((value for key, value in ENTITIES_TO_KEYS.items() if isinstance(entity, key)), None)
-        current_position_cell = self.grid[entity.position.x][entity.position.y][entity_key]
-        if entity.id not in current_position_cell:
-            # The Agent / Package has moved to a new cell. Update the grid with its new position. Remove it from the cell it was in before.
-            previous_position_cell = self.grid[previous_position.x][previous_position.y][entity_key]
-            # Remove the entity from the previous cell it was in.
-            del previous_position_cell[entity.id]
-            # Add the entity to the new cell it is in.
-        self.grid[entity.position.x][entity.position.y][entity_key][entity.id] = object_to_dict(entity)
-
-
-    def update_grid(self, previous_agents_positions: dict, previous_packages_positions: dict, current_iteration: int) -> None:
-        """ Updates the grid with the new positions of the dynamic entities.
-
-        Args:
-            previous_agents_positions (dict): The positions the agents had at the previous iteration.
-            previous_packages_positions (dict): The positions the packages had at the previous iteration.
-            current_iteration (int): The current iteration of the experiment.
-        """        
-        # Dynamic entities that move: Agents & Packages
-        for agent_id, agent_object in self.agents.items():
-            self._update_entity_position(agent_object, previous_agents_positions[agent_id])
-        
-        for package_id, package_object in self.packages.items():
-            self._update_entity_position(package_object, previous_packages_positions[package_id])
-        
         # Dynamic entities that do not move, but can disappear: Obstacles
-        for obstacle_id, obstacle_object in self.obstacles.items():
-            if obstacle_object.iterations_left == -1 and obstacle_id in self.grid[obstacle_object.position.x][obstacle_object.position.y][ENTITIES_TO_KEYS[Obstacle]]:
-                # The object has to disappear from the grid.
-                del self.grid[obstacle_object.position.x][obstacle_object.position.y][ENTITIES_TO_KEYS[Obstacle]][obstacle_id]
-            elif current_iteration == obstacle_object.starting_iteration:
-                # The object has to appear in the grid now.
-                obstacle_object.determine_position(self.grid)
-                self.grid[obstacle_object.position.x][obstacle_object.position.y][ENTITIES_TO_KEYS[Obstacle]][obstacle_id] = object_to_dict(obstacle_object)
+        for obstacle in self.obstacles:
+            if obstacle.iterations_left == -1 and obstacle.pos is not None:
+                # The object has to disappear from the grid. It has already stayed in the environment for the required iterations.
+                self.grid.remove_agent(obstacle)
+            elif current_iteration == obstacle.starting_iteration:
+                # The object has to appear in the grid now, it is the starting iteration.
+                obstacle.determine_position(self.grid)
+                self.grid.place_agent(obstacle, obstacle.pos)
             
 
-    def init_grid(self) -> None:
+    def init_grid(self, agents: List[Agent], package_points: List[PackagePoint], packages: List[Package], obstacles: List[Obstacle]) -> None:
         """ Initializes the grid with the static entities (Package Points, Obstacles and Packages).
         Called only once, at the beginning of the experiment.
         """        
-        for entity in [self.agents, self.package_points, self.packages]:
-            for entity_key, entity_object in entity.items():
-                type_key = next((value for key, value in ENTITIES_TO_KEYS.items() if isinstance(entity_object, key)), None)
-                self.grid[entity_object.position.x][entity_object.position.y][type_key][entity_key] = object_to_dict(entity_object).copy()
-        
-        for obstacle_key, obstacle_object in self.obstacles.items():
-            if obstacle_object.starting_iteration == 1:
-                obstacle_object.determine_position(self.grid)
-                self.grid[obstacle_object.position.x][obstacle_object.position.y][ENTITIES_TO_KEYS[Obstacle]][obstacle_key] = object_to_dict(obstacle_object).copy()
+        for entity in agents + package_points + packages:
+            self.grid.place_agent(entity, entity.pos)
+
+        for obstacle in obstacles:
+            if obstacle.starting_iteration == 1:
+                obstacle.determine_position(self.grid)
+                self.grid.place_agent(obstacle, obstacle.pos)
 
 
     def grid_as_matrix(self, mode:str='dijkstra') -> List[List]:
@@ -138,26 +91,36 @@ class Environment(Model):
         """        
         matrix_grid = []
         if mode == 'dijkstra':
-            return [
-                [0 if self.grid[i][j][PACKAGE_POINT_KEY] or self.grid[i][j][OBSTACLE_KEY] else 1
-                for j in range(self.grid_width)]
-                for i in range(self.grid_height)
-            ]
-        if mode == 'visualization':
             for i in range(self.grid_height):
                 column = []
                 for j in range(self.grid_width):
-                    if self.grid[i][j][PACKAGE_POINT_KEY]:
-                        column.append('x')
-                    elif self.grid[i][j][OBSTACLE_KEY]:
-                        column.append('o')
-                    elif self.grid[i][j][PACKAGE_KEY]:
-                        column.append('p')
-                    elif self.grid[i][j][AGENT_KEY]:
-                        column.append('a')
+                    if self.grid._grid[i][j]:
+                        for entity in self.grid._grid[i][j]:
+                            if isinstance(entity, Obstacle):
+                                column.append(0)
+                            else:
+                                column.append(1)
+                    else:
+                        column.append(1)
+                matrix_grid.append(column)
+        if mode == 'visualization':
+            # Be aware this can add more columns, if more than 1 entity is in the same cell.
+            for i in range(self.grid_height):
+                column = []
+                for j in range(self.grid_width):
+                    if self.grid._grid[i][j]:
+                        for entity in self.grid._grid[i][j]:
+                            if isinstance(entity, PackagePoint):
+                                column.append('x')
+                            if isinstance(entity, Obstacle):
+                                column.append('o')
+                            if isinstance(entity, Package):
+                                column.append('p')
+                            if isinstance(entity, Agent):
+                                column.append('a')
                     else:
                         column.append(' ')
                 matrix_grid.append(column)
 
-            return matrix_grid
+        return matrix_grid
         
