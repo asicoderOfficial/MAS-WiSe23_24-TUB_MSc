e46ca24f563b34260ac988a3606cd9f92a05f3c3
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-23 00:45:51 +0100

    feat/base_code : Dynamic environment creation!

--- src/environment/environment.py
+++ src/environment/environment.py
@@ -1,4 +1,5 @@
 import random
+import math
 from typing import List
 
 from mesa import Model
@@ -6,33 +7,42 @@ from mesa.space import MultiGrid
 
 from src.agents.agent import Agent
 from src.environment.package import Package
-from src.environment.package_point import PACKAGE_POINT_END, PACKAGE_POINT_START, PackagePoint
+from src.environment.package_point import PACKAGE_POINT_END, PACKAGE_POINT_START, PackagePoint, PACKAGE_POINT_INTERMEDIATE
+from src.constants.environment import MAX_PERC_PACKAGE_POINTS
+from src.utils.position import Position
 from src.environment.obstacle import Obstacle, ObstacleCell
 
 
 class Environment(Model):
     """ The environment where the agents interact."""
     def __init__(self, grid_height: int, grid_width: int, agents: List[Agent], \
-                 package_points: List[PackagePoint], obstacles: List[Obstacle], packages: List[Package]) -> None:
+                 starting_package_point: PackagePoint, n_intermediate_package_points: int, n_ending_package_points: int, 
+                 obstacles: List[Obstacle]) -> None:
         """ Constructor.
 
         Args:
             grid_height (int): Height of the grid (number of rows)
             grid_width (int): Width of the grid (number of columns)
             agents (List[Agent]): Agents in the environment.
-            package_points (List[PackagePoint]): Package points in the environment.
             obstacles (List[Obstacle]): Obstacles in the environment.
-            packages (List[Package]): Packages in the environment.
         
         Returns:
             None
         """        
+        if n_intermediate_package_points + n_ending_package_points > grid_height * grid_width * MAX_PERC_PACKAGE_POINTS:
+            raise Exception(f'Too many package points. The maximum number of package points is {grid_height * grid_width * MAX_PERC_PACKAGE_POINTS} for a {grid_height}x{grid_width} grid ({MAX_PERC_PACKAGE_POINTS * 100}% of the grid).')
         self.grid_height = grid_height
         self.grid_width = grid_width
-        self.agents = agents
-        self.package_points = package_points
+
+
+        self.n_intermediate_package_points = n_intermediate_package_points
+        self.n_ending_package_points = n_ending_package_points
+
         self.obstacles = obstacles
-        self.packages = packages
+        self.agents = agents
+        self.starting_package_point = starting_package_point
+        self.intermediate_package_points = []
+        self.ending_package_points = []
         
         self.current_iteration = 1
 
@@ -56,9 +66,7 @@ class Environment(Model):
         for obstacle in self.obstacles:
             obstacle.step(self.current_iteration, self.grid)
             
-        for package_point in self.package_points:
-            if package_point.point_type == PACKAGE_POINT_START and package_point.steps_per_package != None and self.current_iteration - package_point.previous_package_generation_step > package_point.steps_per_package:
-                self.generate_package(package_point)
+        self.starting_package_point.step(self.current_iteration, self.grid, self.ending_package_points)
 
         self.current_iteration += 1
             
@@ -68,29 +76,115 @@ class Environment(Model):
         Called only once, at the beginning of the experiment.
         """        
         # Place static objects for the first time: package points
-        
-        # Place dynamic objects for the first time
-        for entity in self.agents + self.package_points + self.packages:
-            self.grid.place_agent(entity, entity.pos)
+        # Starting package point
+        self.grid.place_agent(self.starting_package_point, self.starting_package_point.pos) 
+        # Intermediate and ending package points
+        # Place them dynamically according to the starting package point position which is supposed to be in the center of the grid
+        # They are created in 'circles', having the most inner circles more probability to have an intermediate package point, and the most outer circles more probability to have an ending package point
+        n_subrectangles = int(math.ceil((self.n_intermediate_package_points + self.n_ending_package_points) / 8))
 
+        n_intermediate_points_by_subrectangle = {}
+        n_ending_points_by_subrectangle = {}
+
+        total_placed_intermediate_points = 0
+        total_placed_ending_points = 0
+
+        for subrectangle in range(1, n_subrectangles + 1):
+            # Randomly decide to place an intermediate package point, so that it is more probable to place it in the first subrectangles (inner part of the grid, closer to starting package point)
+            if total_placed_intermediate_points == self.n_intermediate_package_points and total_placed_ending_points == self.n_ending_package_points:
+                break
+            added_intermediate_points = 0
+            added_ending_points = 0
+            while added_intermediate_points + added_ending_points < 8 and (total_placed_ending_points < self.n_ending_package_points or total_placed_intermediate_points < self.n_intermediate_package_points):
+                intermediate_point_probability = 1 - (subrectangle / n_subrectangles) if subrectangle != n_subrectangles else 0.9
+                ending_point_probability = subrectangle / n_subrectangles if subrectangle != n_subrectangles else 0.1
+                if random.random() < intermediate_point_probability and total_placed_intermediate_points < self.n_intermediate_package_points:
+                    if subrectangle in n_intermediate_points_by_subrectangle:
+                        n_intermediate_points_by_subrectangle[subrectangle] += 1
+                    else:
+                        n_intermediate_points_by_subrectangle[subrectangle] = 1
+                    added_intermediate_points += 1
+                    total_placed_intermediate_points += 1
+                elif random.random() < ending_point_probability and total_placed_ending_points < self.n_ending_package_points:
+                    if subrectangle in n_ending_points_by_subrectangle:
+                        n_ending_points_by_subrectangle[subrectangle] += 1
+                    else:
+                        n_ending_points_by_subrectangle[subrectangle] = 1
+                    added_ending_points += 1
+                    total_placed_ending_points += 1
+
+        for subrectangle in range(1, n_subrectangles + 1):
+            # Randomly decide to place an ending package point, so that it is more probable to place it in the last subrectangles (outer part of the grid)
+            if total_placed_ending_points == self.n_ending_package_points:
+                # All ending package points have been placed
+                break
+            added_ending_points = 0
+            while n_intermediate_points_by_subrectangle[subrectangle] + added_ending_points < 8:
+                probability = subrectangle / n_subrectangles if subrectangle != n_subrectangles else 0.1
+                if total_placed_ending_points < self.n_ending_package_points:
+                    break
+                if probability:
+                    if subrectangle in n_ending_points_by_subrectangle:
+                        n_ending_points_by_subrectangle[subrectangle] += 1
+                    else:
+                        n_ending_points_by_subrectangle[subrectangle] = 1
+                    added_ending_points += 1
+                    total_placed_ending_points += 1
+
+        subrectangles_height = int(math.ceil(self.starting_package_point.pos.x / n_subrectangles))
+        subrectangles_width = int(math.ceil(self.starting_package_point.pos.y / n_subrectangles))
+        for subrectangle in range(1, n_subrectangles + 1):
+            subrectangles_height = subrectangles_height * subrectangle
+            subrectangles_width = subrectangles_width * subrectangle
+            # Generate 8 points in the subrectangle where a package point can be placed, taking as reference the starting package point
+            # Top left
+            top_left = (self.starting_package_point.pos.x - subrectangles_height, self.starting_package_point.pos.y - subrectangles_width)
+            # Upper middle
+            upper_middle = (self.starting_package_point.pos.x - subrectangles_height, self.starting_package_point.pos.y)
+            # Top right
+            top_right = (self.starting_package_point.pos.x - subrectangles_height, self.starting_package_point.pos.y + subrectangles_width)
+            # Middle right
+            middle_right = (self.starting_package_point.pos.x, self.starting_package_point.pos.y + subrectangles_width)
+            # Bottom right
+            bottom_right = (self.starting_package_point.pos.x + subrectangles_height, self.starting_package_point.pos.y + subrectangles_width)
+            # Bottom middle
+            bottom_middle = (self.starting_package_point.pos.x + subrectangles_height, self.starting_package_point.pos.y)
+            # Bottom left
+            bottom_left = (self.starting_package_point.pos.x + subrectangles_height, self.starting_package_point.pos.y - subrectangles_width)
+            # Middle left
+            middle_left = (self.starting_package_point.pos.x, self.starting_package_point.pos.y - subrectangles_width)
+            # All the points in the subrectangle
+            subrectangle_points = [top_left, upper_middle, top_right, middle_right, bottom_right, bottom_middle, bottom_left, middle_left]
+            # Place the package points in the subrectangle
+            n_intermediate_points_in_current_subrectangle = n_intermediate_points_by_subrectangle[subrectangle]
+            # Pick n_intermediate_points_in_current_subrectangle random indices from subrectangle_points list
+            intermediate_points_indices = random.sample(range(len(subrectangle_points)), n_intermediate_points_in_current_subrectangle)
+            # Pick n_ending_points_in_current_subrectangle random indices from subrectangle_points list that are not in intermediate_points_indices
+            ending_points_indices = random.sample([i for i in range(len(subrectangle_points)) if i not in intermediate_points_indices], n_ending_points_by_subrectangle[subrectangle])
+            # Place the package points in the grid
+            for i in range(len(subrectangle_points)):
+                if i in intermediate_points_indices:
+                    # Intermediate package point
+                    pp = PackagePoint(id=f'pp_ss{subrectangle}_{i}', position=Position(subrectangle_points[i][0], subrectangle_points[i][1]), point_type=PACKAGE_POINT_INTERMEDIATE)
+                    self.grid.place_agent(pp, pp.pos)
+                    self.intermediate_package_points.append(pp)
+                elif i in ending_points_indices:
+                    # Ending package point
+                    pp = PackagePoint(id=f'pp_ss{subrectangle}_{i}', position=Position(subrectangle_points[i][0], subrectangle_points[i][1]), point_type=PACKAGE_POINT_END)
+                    self.grid.place_agent(pp, pp.pos)
+                    self.ending_package_points.append(pp)
+
+        # Place dynamic objects for the first time
         for obstacle in self.obstacles:
             obstacle.step(self.current_iteration, self.grid)
+
+        for agent in self.agents:
+            if agent.pos is None:
+                # The agent has not been placed in the grid yet
+                # Place it in a random intermediate package point, as the position has not been specified (and we assume it will be the starting package point position)
+                agent.pos = random.choice([pp.pos for pp in self.intermediate_package_points])
         
         self.current_iteration += 1
-        
-                
-
-    def generate_package(self, package_point: PackagePoint) -> None:
-        # Choose random destination
-        destination = random.choice([pp for pp in self.package_points if pp.point_type == PACKAGE_POINT_END and pp.id != package_point.id])
-        
-        # Create package and place it on grid
-        package = Package(f'p{len(self.packages)}', package_point.pos, destination.pos, 10)
-        self.grid.place_agent(package, package_point.pos)
-        self.packages.append(package)
-        print("Generated package")
-        # Update package point
-        package_point.previous_package_generation_step = self.current_iteration
         
 
     def grid_as_matrix(self, mode:str='dijkstra') -> List[List]:
@@ -129,7 +223,7 @@ class Environment(Model):
                                 cell += 'p'
                             if isinstance(entity, Agent):
                                 cell += 'a'
-                            else:
+                            if isinstance(entity, ObstacleCell):
                                 cell += 'o'
                         column.append(cell)
                     else:
