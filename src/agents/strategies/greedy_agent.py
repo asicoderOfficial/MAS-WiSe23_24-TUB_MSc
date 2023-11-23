ff466e02b837956cc0358548a832ab0219975dd0
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-23 23:53:37 +0100

    strategies: GreedyAgent checkpoint

--- src/agents/strategies/greedy_agent.py
+++ src/agents/strategies/greedy_agent.py
@@ -1,5 +1,5 @@
 import random
-from typing import Union
+from typing import Union, List
 from src.agents.agent import Agent
 from src.agents.perception import Perception
 from src.environment.package import Package
@@ -9,7 +9,7 @@ from src.utils.grid2matrix import convert_grid_to_matrix
 
 
 class GreedyAgent(Agent):
-    def __init__(self, id: str, position: Position, package: Union[Package, None], 
+    def __init__(self, id: str, position: Position, packages: List[Package], 
                  perception: Perception, algorithm_name: str, decision: int=0) -> None:
         """Chain Agent delivers package only to specific package point type and returns to origin position after delivering package.
 
@@ -22,28 +22,30 @@ class GreedyAgent(Agent):
             perception (Perception): The subgrid the agent is currently perceiving.
             package_point_type (str): Agent's goal package point type, agent will deliver package only to this type of package point.
         """
-        super().__init__(id, position, package, perception, algorithm_name)
+        super().__init__(id, position, packages, perception, algorithm_name)
         self.goal_package = None
         self.decision = decision
         self.previous_point = None
 
 
     def step(self, grid) -> None:
+        perception = self.perception.percept(self.pos, grid)
         if self.goal_package:
-            if self.goal_package.destination.pos.x == self.pos.x and self.goal_package.destination.pos.y == self.pos.y:
+            if self.goal_package.destination.x == self.pos.x and self.goal_package.destination.y == self.pos.y:
                 # We are in the destination, deliver package
                 self.deliver_package(self.goal_package, self.goal_package.destination, grid)
                 self.goal_package = None
             else:
                 # We are carrying a package, but still did not reach the destination
                 # Move towards the destination using the algorithm specified in the constructor
-                next_pos = self.get_next_position(grid, perception, self.goal_package.destination.pos, self.origin_point)
+                print(self.goal_package)
+                print(self.goal_package.destination)
+                next_pos = self.get_next_position(grid, perception, self.goal_package.destination, '')
                 self.move(next_pos, perception, grid)
-        perception = self.perception.percept(self.pos, grid)
-        visible_packages_available = [package for package in perception.visible_packages if not package.picked]
+        visible_packages_available = [item for items in perception.values() for item in items if isinstance(item, Package) and item.picked == False]
         if not visible_packages_available:
             # Do a random move
-            self.move(Position(self.pos.x + random.randint(-1, 1), self.pos.y + random.randint(-1, 1)), perception, grid)
+            self.random_move(grid, perception)
         else:
             # Select the package based on decision parameter:
             # 0 - closest package to the agent
@@ -53,34 +55,54 @@ class GreedyAgent(Agent):
             if self.decision == 0:
                 distances_to_agent = []
                 for package in visible_packages_available:
-                    distances_to_agent.append((package, package.dist_to(self.pos)))
+                    distances_to_agent.append((package, package.pos.dist_to(self.pos)))
                 distances_to_agent.sort(key=lambda x: x[1])
-                self.chosen_package = distances_to_agent[0][0]
+                self.goal_package = distances_to_agent[0][0]
             elif self.decision == 1:
                 distances_to_agent = []
                 for package in visible_packages_available:
                     if not package.is_delayed:
-                        distances_to_agent.append((package, package.dist_to(self.pos)))
+                        distances_to_agent.append((package, package.pos.dist_to(self.pos)))
                 distances_to_agent.sort(key=lambda x: x[1])
-                self.chosen_package = distances_to_agent[0][0]
+                self.goal_package = distances_to_agent[0][0]
             elif self.decision == 2:
                 iterations_to_be_delayed = []
                 for package in visible_packages_available:
                     if not package.is_delayed:
                         iterations_to_be_delayed.append((package, package.max_iterations_to_deliver - package.iterations))
                 iterations_to_be_delayed.sort(key=lambda x: x[1])
-                self.chosen_package = iterations_to_be_delayed[0][0]
+                self.goal_package = iterations_to_be_delayed[0][0]
             elif self.decision == 3:
                 distances_to_destination = []
                 for package in visible_packages_available:
-                    distances_to_destination.append((package, package.dist_to(package.destination.pos)))
+                    distances_to_destination.append((package, package.pos.dist_to(package.destination.pos)))
                 distances_to_destination.sort(key=lambda x: x[1])
-                self.chosen_package = distances_to_destination[0][0]
+                self.goal_package = distances_to_destination[0][0]
         
-        if not self.chosen_package:
+        if not self.goal_package:
             # Do a random move
-            self.move(Position(self.pos.x + random.randint(-1, 1), self.pos.y + random.randint(-1, 1)), perception, grid)
+            self.random_move(grid, perception)
          
+    def random_move(self, grid, perception) -> None:
+        if self.pos.x == 0 and self.pos.y == 0:
+            self.move(Position(self.pos.x + random.randint(0, 1), self.pos.y + random.randint(0, 1)), perception, grid)
+        elif self.pos.x == 0 and self.pos.y == grid.height - 1:
+            self.move(Position(self.pos.x + random.randint(0, 1), self.pos.y + random.randint(-1, 0)), perception, grid)
+        elif self.pos.x == grid.width - 1 and self.pos.y == 0:
+            self.move(Position(self.pos.x + random.randint(-1, 0), self.pos.y + random.randint(0, 1)), perception, grid)
+        elif self.pos.x == grid.width - 1 and self.pos.y == grid.height - 1:
+            self.move(Position(self.pos.x + random.randint(-1, 0), self.pos.y + random.randint(-1, 0)), perception, grid)
+        elif self.pos.x == 0:
+            self.move(Position(self.pos.x + random.randint(0, 1), self.pos.y + random.randint(-1, 1)), perception, grid)
+        elif self.pos.x == grid.width - 1:
+            self.move(Position(self.pos.x + random.randint(-1, 0), self.pos.y + random.randint(-1, 1)), perception, grid)
+        elif self.pos.y == 0:
+            self.move(Position(self.pos.x + random.randint(-1, 1), self.pos.y + random.randint(0, 1)), perception, grid)
+        elif self.pos.y == grid.height - 1:
+            self.move(Position(self.pos.x + random.randint(-1, 1), self.pos.y + random.randint(-1, 0)), perception, grid)
+        else:
+            self.move(Position(self.pos.x + random.randint(-1, 1), self.pos.y + random.randint(-1, 1)), perception, grid)
+
 
     def get_next_position(self, grid, perception, destination: Position, destination_type: str) -> Position:
         if self.algorithm_name == 'dijkstra':
