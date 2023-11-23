17a9e162867dff4de8a347be82723d4f3fd55bc6
Maxim Popov <itsmail4max@gmail.com>
2023-11-23 10:48:09 +0100

    small fixes in chain agent

--- src/agents/chain_agent.py
+++ src/agents/chain_agent.py
@@ -22,52 +22,54 @@ class ChainAgent(Agent):
             package_point_type (str): Agent's goal package point type, agent will deliver package only to this type of package point.
         """
         super().__init__(id, position, package, perception, algorithm_name)
-        self.goal_package_point = goal_package_point
+        self.goal_package_point_type = goal_package_point
         self.previous_point = None
         if goal_package_point == PACKAGE_POINT_INTERMEDIATE:
-            self.origin_point = PACKAGE_POINT_START
+            self.origin_point_type = PACKAGE_POINT_START
         elif goal_package_point == PACKAGE_POINT_END:
-            self.origin_point = PACKAGE_POINT_INTERMEDIATE
+            self.origin_point_type = PACKAGE_POINT_INTERMEDIATE
         
     def step(self, grid) -> None:
         perception = self.perception.percept(self.pos, grid)
         
         if len(self.packages) > 0:
-            valid_package_points = [pp for pp in self.perception.visible_package_points if pp.point_type == self.goal_package_point]
-            for package_point in valid_package_points:
+            valid_package_points = [pp for pp in self.perception.visible_package_points if pp.point_type == self.goal_package_point_type and pp.pos == self.pos]
+            if len(valid_package_points) > 0:
+                package_point = valid_package_points[0]
                 # Correct package point on current position, deliver package
-                if self.goal_package_point == package_point.point_type \
-                    and (package_point.point_type == PACKAGE_POINT_INTERMEDIATE or (self.goal_package_point == PACKAGE_POINT_END and self.packages[0].destination == package_point.pos)):
+                if self.goal_package_point_type == package_point.point_type \
+                    and (package_point.point_type == PACKAGE_POINT_INTERMEDIATE and (self.packages[0].destination != None or self.packages[0].destination == package_point.pos) \
+                         or (self.goal_package_point_type == PACKAGE_POINT_END and self.packages[0].destination == package_point.pos)):
                     # Found searched package point, deliver package
                     if self.pos == package_point.pos:
                         self.previous_point = self.packages[0].destination
-                        self.previous_point_type = self.goal_package_point
+                        self.previous_point_type = self.goal_package_point_type
                         for package in self.packages:
                             self.deliver_package(package, package_point, grid)
                         return   
             # Search for path to package point
-            if self.goal_package_point == PACKAGE_POINT_INTERMEDIATE and self.packages[0].intermediate_point_pos != None:
+            if self.goal_package_point_type == PACKAGE_POINT_INTERMEDIATE and self.packages[0].intermediate_point_pos != None:
                 destination = self.packages[0].intermediate_point_pos
             else:
                 destination = self.packages[0].destination
-            next_pos = self.get_next_position(grid, perception, destination, self.goal_package_point)
+            next_pos = self.get_next_position(grid, perception, destination, self.goal_package_point_type)
             self.move(next_pos, perception, grid)
         else:
             if self.origin == self.pos:
                 # Package point should be on current position, pick up package
                 current_packages = [package for package in perception[self.pos.to_tuple()] if isinstance(package, Package) and not package.picked]
-                current_pp = []
                 if len(current_packages) > 0:
-                    packages = self.get_biggest_package_order(current_packages, self.goal_package_point)
+                    package_order = self.get_biggest_package_order(current_packages, self.goal_package_point_type)
                     self.previous_point = self.origin
-                    self.previous_point_type = self.origin_point
-                    for package in current_packages:
+                    self.previous_point_type = self.origin_point_type
+                    for package in package_order:
                         self.pick_package(package, grid)
+                    print("Picked up", len(current_packages), "packages")
                 else:
                     print("Agent is at package point, but there are no packages, resting...")
             else:
                 # Search for path to origin
-                next_pos = self.get_next_position(grid, perception, self.origin, self.origin_point)
+                next_pos = self.get_next_position(grid, perception, self.origin, self.origin_point_type)
                 self.move(next_pos, perception, grid)
 
     
