7abeeb9d9eb3244f4f8e13f94f587c80a1a7b5b8
asicoderOfficial <asicoderofficial@gmail.com>
2023-11-24 22:36:24 +0100

    Fix imports

--- src/agents/path_algorithms/pheromone.py
+++ src/agents/path_algorithms/pheromone.py
@@ -1,5 +1,5 @@
 from typing import List
-from agents.path_algorithms.path_algorithm import PathAlgorithm
+from src.agents.path_algorithms.path_algorithm import PathAlgorithm
 from src.environment.obstacle import Obstacle
 from src.utils.position import Position
 import random
--- src/environment/environment.py
+++ src/environment/environment.py
@@ -4,7 +4,7 @@ from typing import List
 
 from mesa import Model
 from mesa.space import MultiGrid
-from agents.strategies.chain_agent import ChainAgent
+from src.agents.strategies.chain_agent import ChainAgent
 from src.agents.strategies.greedy_agent import GreedyAgent
 
 from src.agents.agent import Agent
