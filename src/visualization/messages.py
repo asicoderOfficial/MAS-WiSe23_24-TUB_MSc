544fa9d441bb4527b4c2c96d70d06ad75dbbe599
asicoderOfficial <asicoderofficial@gmail.com>
2024-01-15 18:42:02 +0100

    Plots: number of messages done

--- src/visualization/messages.py
+++ src/visualization/messages.py
@@ -42,9 +42,9 @@ def number_of_messages(csvs_path: str, store_path:str) -> Union[int, pd.DataFram
     messages_df = pd.DataFrame(messages_data).T.rename(columns={'index':'experiment', 0:'n_messages', 1:'n_intermediate_pickup_requests', 2:'n_ending_pickup_requests', 3:'n_intermediate_acceptances', 4:'n_ending_acceptances', 5:'n_intermediate_notifications', 6:'n_ending_notifications'})
 
     fig = go.Figure()
-    #fig.add_trace(go.Scatter(x=messages_df.index, y=messages_df['n_messages'], mode='lines+markers', name='Total messages'))
-    #fig.add_trace(go.Scatter(x=messages_df.index, y=messages_df['n_intermediate_pickup_requests'], mode='lines+markers', name='Intermediate pickup requests'))
-    #fig.add_trace(go.Scatter(x=messages_df.index, y=messages_df['n_ending_pickup_requests'], mode='lines+markers', name='Ending pickup requests'))
+    fig.add_trace(go.Scatter(x=messages_df.index, y=messages_df['n_messages'], mode='lines+markers', name='Total messages'))
+    fig.add_trace(go.Scatter(x=messages_df.index, y=messages_df['n_intermediate_pickup_requests'], mode='lines+markers', name='Intermediate pickup requests'))
+    fig.add_trace(go.Scatter(x=messages_df.index, y=messages_df['n_ending_pickup_requests'], mode='lines+markers', name='Ending pickup requests'))
     fig.add_trace(go.Scatter(x=messages_df.index, y=messages_df['n_intermediate_acceptances'], mode='lines+markers', name='Intermediate acceptances'))
     fig.add_trace(go.Scatter(x=messages_df.index, y=messages_df['n_ending_acceptances'], mode='lines+markers', name='Ending acceptances'))
 
@@ -54,17 +54,4 @@ def number_of_messages(csvs_path: str, store_path:str) -> Union[int, pd.DataFram
     fig.update_layout(legend_title_text='Message type')
     fig = general_styling(fig)
     fig.write_image(store_path)
-
-
-def average_delivery_time(csvs_path:str, store_path:str) -> Union[int, pd.DataFrame]:
-    csv_files = [f for f in search_files_recursively(csvs_path, '.csv')  if 'messages' in f]
-    #average_delivery_time = { csv_file.split('/')[-2].split('-')[0]: pd.read_csv(csv_file)['
-    #average_delivery_time = pd.DataFrame.from_dict(average_delivery_time, orient='index', columns=['average_delivery_time'])
-
-    """
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=average_delivery_time.index, y=average_delivery_time['average_delivery_time'], mode='lines+markers', name='lines+markers'))
-    fig.update_layout(title_text='Average delivery time per algorithm')
-    fig.write_image(store_path)
-    """
 
